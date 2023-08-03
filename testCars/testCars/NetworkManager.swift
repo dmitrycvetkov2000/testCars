4fdc80c29ceff9cb9720f58927e71a5bcf8650a8
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2023-08-03 04:41:33 +0300

    Created Network Manager

--- testCars/testCars/NetworkManager.swift
+++ testCars/testCars/NetworkManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+import Foundation
+
+protocol NetworkManagerProtocol {
+    func getAuto(completion: @escaping (Result<[MainCar]?, Error>) -> Void)
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    func getAuto(completion: @escaping (Result<[MainCar]?, Error>) -> Void) {
+        let urlString = "http://am111.05.testing.place/api/v1/cars/list"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            do {
+                let obj = try JSONDecoder().decode([MainCar].self, from: data!)
+                completion(.success(obj))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
