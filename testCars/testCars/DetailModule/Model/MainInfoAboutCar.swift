ac21179d0b4772cde1f744db285e2baf9ac52fae
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2023-08-03 22:29:30 +0300

    create model for DetailVC

--- testCars/testCars/DetailModule/Model/MainInfoAboutCar.swift
+++ testCars/testCars/DetailModule/Model/MainInfoAboutCar.swift
@@ -1,51 +1,66 @@
 //
-//  MainCar.swift
+//  MainInfoAboutCar.swift
 //  testCars
 //
-//  Created by Дмитрий Цветков on 02.08.2023.
+//  Created by Дмитрий Цветков on 03.08.2023.
 //
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let mainInfoAboutCar = try? JSONDecoder().decode(MainInfoAboutCar.self, from: jsonData)
 
 import Foundation
 
-// MARK: - MainCar
-struct MainCar: Decodable {
+// MARK: - MainInfoAboutCar
+struct MainInfoAboutCar: Codable {
+    let car: Car?
+    let user: User?
+}
+
+// MARK: - Car
+struct Car: Codable {
     let id, forSale: Int?
-    let brandName, modelName, engineName: String?
+    let brandName, modelName: String?
     let year: Int?
     fileprivate let price: JSONNull?
     let brandID, modelID, engineID, transmissionID: Int?
-    let placeID, name: String?
-    let image, thumbnail: String?
-    let cityName, countryName: String?
-    let transmissionName: TransmissionName?
-    let engineVolume, placeName: String?
+    let placeID, name, cityName, countryName: String?
+    let transmissionName, placeName: String?
     fileprivate let images: [Image]?
-    let engine: String?
+    let inSelectionCount, followersCount: Int?
+    let follow: Bool?
+    let engine, engineName, engineVolume: String?
+    let isModerated: Bool?
 
     enum CodingKeys: String, CodingKey {
         case id
         case forSale = "for_sale"
         case brandName = "brand_name"
         case modelName = "model_name"
-        case engineName = "engine_name"
         case year, price
         case brandID = "brand_id"
         case modelID = "model_id"
         case engineID = "engine_id"
         case transmissionID = "transmission_id"
         case placeID = "place_id"
-        case name, image, thumbnail
+        case name
         case cityName = "city_name"
         case countryName = "country_name"
         case transmissionName = "transmission_name"
-        case engineVolume = "engine_volume"
         case placeName = "place_name"
-        case images, engine
+        case images
+        case inSelectionCount = "in_selection_count"
+        case followersCount = "followers_count"
+        case follow, engine
+        case engineName = "engine_name"
+        case engineVolume = "engine_volume"
+        case isModerated = "is_moderated"
     }
 }
 
 // MARK: - Image
-fileprivate struct Image: Decodable {
+fileprivate struct Image: Codable {
     let id: Int?
     let isPrimary: Bool?
     let size, index: Int?
@@ -60,16 +75,30 @@ fileprivate struct Image: Decodable {
     }
 }
 
-enum TransmissionName: String, Decodable {
-    case at = "AT"
-    case mt = "MT"
+// MARK: - User
+struct User: Codable {
+    let id: Int?
+    let username, email, about: String?
+    let avatar: Avatar?
+    let autoCount: Int?
+    let mainAutoName: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, username, email, about, avatar
+        case autoCount = "auto_count"
+        case mainAutoName = "main_auto_name"
+    }
 }
 
-typealias MainCars = [MainCar]
+// MARK: - Avatar
+struct Avatar: Codable {
+    let path: String?
+    let url: String?
+}
 
 // MARK: - Encode/decode helpers
 
-fileprivate class JSONNull: Decodable, Hashable {
+fileprivate class JSONNull: Codable, Hashable {
 
     public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
         return true
@@ -93,3 +122,4 @@ fileprivate class JSONNull: Decodable, Hashable {
         try container.encodeNil()
     }
 }
+
